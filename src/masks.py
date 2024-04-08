e18afd2183431ee3c9ce87268a40928ae7f57c71
Bogdanchik <bogdanleontev59@gmail.com>
2024-04-08 10:30:24 +0300

    added function convert_date

--- src/masks.py
+++ src/masks.py
@@ -9,7 +9,11 @@ def mask_card_number(user_card_number: str) -> str:
 def mask_account_number(user_account_number: str) -> str:
     """
     Функция принимает номер счета и возвращает маску счета.
-
     """
     masked_number = "**" + user_account_number[-4:]
     return masked_number
+
+
+def convert_date(input_date):
+    date_parts = input_date.split('T')[0].split('-')
+    return f"{date_parts[2]}.{date_parts[1]}.{date_parts[0]}"
--- tests/__init__.py
+++ tests/__init__.py
@@ -1,8 +1,10 @@
-from src.masks import mask_account_number, mask_card_number
+from src.masks import mask_account_number, mask_card_number, convert_date
 
-user_input = input()
+user_input = input("Карта/счет?   ")
 
 if "Счет" in user_input:
     print(f"Счет {mask_account_number(user_input)}")
 else:
     print(f"{mask_card_number(user_input)}")
+
+print(convert_date(input("время?   ")))
