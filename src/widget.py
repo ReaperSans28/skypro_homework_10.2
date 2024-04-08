930fab80e0f5cc4e4492047ee43d27ecaff49afb
Bogdanchik <bogdanleontev59@gmail.com>
2024-04-08 11:39:27 +0300

    added docstring for functions in widget.py

--- src/widget.py
+++ src/widget.py
@@ -2,11 +2,17 @@ from src.masks import mask_account_number, mask_card_number
 
 
 def convert_date(input_date):
+    """
+    Функция выдает нам дату.
+    """
     date_parts = input_date.split("T")[0].split("-")
     return f"{date_parts[2]}.{date_parts[1]}.{date_parts[0]}"
 
 
 def number_or_account(user_input):
+    """
+    Функция определяет работаем мы с счетом или картой.
+    """
     if "Счет" in user_input:
         print(f"Счет {mask_account_number(user_input)}")
     else:
